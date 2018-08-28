63d85542bbedbfa0997cfee1178a49e2c52149b2
Daisy Dai <893487287@qq.com>
2018-08-28 18:32:39 +0800

    Add api function to Vue root

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import api from './api/index.js'
+// Register api as Vue prototype.
+Vue.prototype.$api = api
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
